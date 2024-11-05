7eeaa8f9351791ede284a9be416091f5bc51e98b
Shulin Ji <shulin.ji@mail.utoronto.ca>
2024-11-05 01:08:52 -0500

    Create 271. Encode and Decode Strings.py

---
+++ 271. Encode and Decode Strings.py
@@ -0,0 +1,90 @@
+# Design an algorithm to encode a list of strings to a string. The encoded string is then sent over the network and is decoded back to the original list of strings.
+
+# Machine 1 (sender) has the function:
+
+# string encode(vector<string> strs) {
+#   // ... your code
+#   return encoded_string;
+# }
+# Machine 2 (receiver) has the function:
+# vector<string> decode(string s) {
+#   //... your code
+#   return strs;
+# }
+# So Machine 1 does:
+
+# string encoded_string = encode(strs);
+# and Machine 2 does:
+
+# vector<string> strs2 = decode(encoded_string);
+# strs2 in Machine 2 should be the same as strs in Machine 1.
+
+# Implement the encode and decode methods.
+
+# You are not allowed to solve the problem using any serialize methods (such as eval).
+
+class Codec:
+    def encode(self, strs):
+        """
+        Encodes a list of strings to a single string.
+
+        :param strs: List of strings to encode.
+        :return: Encoded string.
+        """
+        # Initialize an empty string to hold the encoded strings
+        encoded_string = ''
+
+        # Iterate over each string in the input list
+        for s in strs:
+            # Replace each occurrence of '/' with '//'
+            # This is our way of "escaping" the slash character
+            # Then add our delimiter '/:' to the end
+            encoded_string += s.replace('/', '//') + '/:'
+
+        # Return the final encoded string
+        return encoded_string
+
+    def decode(self, s):
+        """
+        Decodes a single string to a list of strings.
+
+        :param s: String to decode.
+        :return: List of decoded strings.
+        """
+        # Initialize an empty list to hold the decoded strings
+        decoded_strings = []
+
+        # Initialize a string to hold the current string being built
+        current_string = ""
+
+        # Initialize an index 'i' to start of the string
+        i = 0
+
+        # Iterate while 'i' is less than the length of the encoded string
+        while i < len(s):
+            # If we encounter the delimiter '/:'
+            if s[i:i+2] == '/:':
+                # Add the current_string to the list of decoded_strings
+                decoded_strings.append(current_string)
+
+                # Clear current_string for the next string
+                current_string = ""
+
+                # Move the index 2 steps forward to skip the delimiter
+                i += 2
+
+            # If we encounter an escaped slash '//'
+            elif s[i:i+2] == '//':
+                # Add a single slash to the current_string
+                current_string += '/'
+
+                # Move the index 2 steps forward to skip the escaped slash
+                i += 2
+
+            # Otherwise, just add the character to current_string
+            else:
+                current_string += s[i]
+                i += 1
+
+        # Return the list of decoded strings
+        return decoded_strings
