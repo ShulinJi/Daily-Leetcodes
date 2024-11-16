b01f3161f287ab1aa64c21ca621f7eb613a7163d
Shulin Ji <shulin.ji@mail.utoronto.ca>
2024-11-16 15:54:20 -0500

    Update 100. Same Tree.py

--- Binary Tree General/100. Same Tree.py
+++ Binary Tree General/100. Same Tree.py
@@ -9,17 +9,26 @@ from collections import deque
 
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        # use dequeue because it has both O(1) for pop from left and O(1) for pop from right
         queue = deque([p, q])
         while queue:
+            # pop first two elements
             p = queue.popleft()
             q = queue.popleft()
 
+            # If both of the values are None, we continue checking
             if not p and not q:
                 continue
             
+            # if values are different, then return False
+            # if one of them is None, and other is not (we have covered case when both are None),
+            # then structure is different
             if not p or not q or p.val != q.val:
                 return False
 
+            # we add both left sides and right sides to the queue
+            # as a result, we are checking the BFS left side first, after checking all of the left tree
+            # then we start checking the whole right tree
             queue.append(p.left)
             queue.append(q.left)
             queue.append(p.right)
