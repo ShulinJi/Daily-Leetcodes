b8e82186e3d4d228ba31abfb39b8da18665df71d
Shulin Ji <shulin.ji@mail.utoronto.ca>
2024-11-20 22:18:31 -0500

    Update 207. Course Schedule.py

--- Graph Theory (BFS, DFS)/207. Course Schedule.py
+++ Graph Theory (BFS, DFS)/207. Course Schedule.py
@@ -22,18 +22,26 @@
 # DFS Solution, Runtime O(M+N), Space O(M+N)
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        # DFS to find if there's a cycle in the graph by finding if the node on recursion stack is the one we are checking
+        # Also, we track visited, if we have visited the node, we continue to the next
         def dfs(course_number, adj, visited, inStack):
+            # If the node we are checking is on stack, we have a cycle
             if inStack[course_number] == True:
                 return True
+            # if the node we have already checked, we continue
             if visited[course_number] == True:
                 return False
             
+            # Set the current node to stack and visited
             visited[course_number] = True
             inStack[course_number] = True
+            # Check the branch of the node if there's any cycle in it
+            # If so, return True, we find a cycle 
             for course in adj[course_number]:
                 if dfs(course, adj, visited, inStack):
                     return True
             
+            # We remove the current node from the stack since we finished checking its branch
             inStack[course_number] = False
             return False
 
@@ -44,9 +52,13 @@ class Solution:
         for prerequisite in prerequisites:
             adj[prerequisite[1]].append(prerequisite[0])
         
+        # Create visited and inStack for DFS use
+        # Visited: track the node we have visited
+        # inStack: track the DFS recursion stack, if the node is on Stack, we make it True
         visited = [False] * numCourses
         inStack = [False] * numCourses
         for i in range(numCourses):
+            # If there is a cycle in a graph, we return False
             if dfs(i, adj, visited, inStack):
                 return False
         return True
