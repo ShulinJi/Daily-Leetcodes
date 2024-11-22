9e43405cc141d1113d2087453a0d060fc868e8e3
Shulin Ji <shulin.ji@mail.utoronto.ca>
2024-11-21 21:06:37 -0500

    Update 210. Course Schedule II.py

--- Graph Theory (BFS, DFS)/210. Course Schedule II.py
+++ Graph Theory (BFS, DFS)/210. Course Schedule II.py
@@ -1,3 +1,37 @@
+# Exmaple of Topological Sort! (logical, follow the arrow, check the the arrows(dependencies) of a node and then check others)
+# Simple Example
+# Imagine you have a list of tasks to complete, but some tasks can’t be done until others are finished. For example:
+
+# Tasks:
+
+# Task A: Buy ingredients
+# Task B: Cook dinner
+# Task C: Set the table
+# Task D: Eat dinner
+# Dependencies:
+
+# You can’t cook dinner (Task B) until you buy ingredients (Task A).
+# You can’t set the table (Task C) until you’ve cooked dinner (Task B).
+# You can’t eat dinner (Task D) until you’ve set the table (Task C).
+# Order:
+
+# The only way to do these tasks in the right order is: A → B → C → D
+# This is topological sorting: arranging tasks so that all dependencies are respected.
+
+# Key Idea
+# Graph Representation:
+
+# Tasks are like nodes.
+# Dependencies are like arrows pointing from one task to another (e.g., "A → B" means "Do A before B").
+# Topological Sort:
+
+# It’s the process of finding an order to do all the tasks so that:
+# No task is done before its prerequisites are complete.
+# All tasks are completed in a valid order.
+
+
+
+
 # There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1. You are given an array prerequisites where prerequisites[i] = [ai, bi] indicates that you must take course bi first if you want to take course ai.
 
 # For example, the pair [0, 1], indicates that to take course 0 you have to first take course 1.
@@ -21,22 +55,32 @@
 
 
 class Solution:
+    # The three phase of a node, WHITE is not visited, GRAY is on stack, BLACK means its branch and neighbors are checked
     WHITE = 1
     GRAY = 2
     BLACK = 3
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         def dfs(x):
+            # if we find a cycle, we simply just return
             nonlocal is_possible
             if is_possible == False:
                 return
+
+            # we set current node on stack
             color[x] = Solution.GRAY
+            # Check for neighbors of current node
             if x in adj:
                 for i in adj[x]:
+                    # if the neighbor is on stack, it means we have a cycle
+                    # else, we simply perform check
                     if color[i] == Solution.GRAY:
                         is_possible = False
                     elif color[i] == Solution.WHITE:
                         dfs(i)
+
+            # we have checked the node and its branch and neighbors
             color[x] = Solution.BLACK
+            # we append the topological order
             topological_sorted_order.append(x)
 
 
@@ -49,6 +93,7 @@ class Solution:
         topological_sorted_order = []
         is_possible = True
         for x in range(numCourses):
+            # perform dfs if current node is not visited yet
             if color[x] == Solution.WHITE:
                 dfs(x)
         
