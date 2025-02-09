191a7c5f3ced692ac3c57455c6b2d3936cbbfca5
Shulin Ji <shulin.ji@mail.utoronto.ca>
2025-02-09 01:50:34 -0500

    added solutions

--- Array_or_String/309. Best Time to Buy and Sell Stock with Cooldown.py
+++ Array_or_String/309. Best Time to Buy and Sell Stock with Cooldown.py
@@ -12,3 +12,22 @@
 # Input: prices = [1]
 # Output: 0
 
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        sold, held, reset = float('-inf'), float('-inf'), 0
+
+        for price in prices:
+            # Alternative: the calculation is done in parallel.
+            # Therefore no need to keep temporary variables
+            #sold, held, reset = held + price, max(held, reset-price), max(reset, sold)
+
+            pre_sold = sold
+            sold = held + price
+            held = max(held, reset - price)
+            reset = max(reset, pre_sold)
+
+        return max(sold, reset)
