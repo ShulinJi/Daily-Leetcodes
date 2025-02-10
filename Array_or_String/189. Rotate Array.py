7ba55c22847138a8b26421bc9bdac831091ea947
Shulin Ji <shulin.ji@mail.utoronto.ca>
2025-02-09 23:58:02 -0500

    better space

--- Array_or_String/189. Rotate Array.py
+++ Array_or_String/189. Rotate Array.py
@@ -18,6 +18,20 @@
 
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
+    # O(n) runtime, O(1) space
+    # Reverse method
+    def reverse(self, nums: list, start: int, end: int) -> None:
+        while start < end:
+            nums[start], nums[end] = nums[end], nums[start]
+            start, end = start + 1, end - 1
+
+    def rotate(self, nums: List[int], k: int) -> None:
+        n = len(nums)
+        k %= n
+
+        self.reverse(nums, 0, n - 1)
+        self.reverse(nums, 0, k - 1)
+        self.reverse(nums, k, n - 1)
 
 
 
