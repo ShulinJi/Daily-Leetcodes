b347f78a5337dc5e06f9ac1c112abb5b2bf747a8
Shulin Ji <shulin.ji@mail.utoronto.ca>
2025-02-25 22:35:41 -0500

    added solutions two pointer

--- Array_or_String/Two Pointer/15. 3Sum.py
+++ Array_or_String/Two Pointer/15. 3Sum.py
@@ -21,3 +21,34 @@
 # Input: nums = [0,0,0]
 # Output: [[0,0,0]]
 # Explanation: The only possible triplet sums up to 0.
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        # O(N^2) runtime, and O(nlogn) to O(N) space
+        returnList = []
+        nums.sort()
+        for i in range(len(nums)):
+            # ignore the duplicate
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            
+            complement = 0 - nums[i]
+            left = i + 1
+            right = len(nums) - 1
+            while left < right:
+                # the sum is bigger than complement, decrease right
+                if nums[left] + nums[right] > complement:
+                    right -= 1
+                elif nums[left] + nums[right] < complement:
+                    left += 1
+                # we find the match
+                else:
+                    returnList.append([nums[i], nums[left], nums[right]])
+                    left += 1
+                    right -= 1
+                    # we skip the duplicates here
+                    while left < right and nums[left] == nums[left - 1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right + 1]:
+                        right -= 1
+        return returnList
