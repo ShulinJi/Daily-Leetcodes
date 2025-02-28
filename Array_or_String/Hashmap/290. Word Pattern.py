a90ba25b7f5e1231aa7cae62766958fd51069496
Shulin Ji <shulin.ji@mail.utoronto.ca>
2025-02-27 22:45:35 -0500

    added word pattern

---
+++ Array_or_String/Hashmap/290. Word Pattern.py
@@ -0,0 +1,50 @@
+# Given a pattern and a string s, find if s follows the same pattern.
+# Here follow means a full match, such that there is a bijection between a letter in pattern and a non-empty word in s. Specifically:
+# Each letter in pattern maps to exactly one unique word in s.
+# Each unique word in s maps to exactly one letter in pattern.
+# No two letters map to the same word, and no two words map to the same letter.
+
+# Example 1:
+# Input: pattern = "abba", s = "dog cat cat dog"
+# Output: true
+# Explanation:
+# The bijection can be established as:
+# 'a' maps to "dog".
+# 'b' maps to "cat".
+
+# Example 2:
+# Input: pattern = "abba", s = "dog cat cat fish"
+# Output: false
+# Example 3:
+# Input: pattern = "aaaa", s = "dog cat cat dog"
+# Output: false
+
+# Constraints:
+
+# 1 <= pattern.length <= 300
+# pattern contains only lower-case English letters.
+# 1 <= s.length <= 3000
+# s contains only lowercase English letters and spaces ' '.
+# s does not contain any leading or trailing spaces.
+# All the words in s are separated by a single space.
+
+
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        # split the string to words -> the array of same size
+        words = s.split()
+        if len(words) != len(pattern):
+            return False
+
+        # create two hashmaps to determine if 
+        hashmap_pattern_to_s = {}
+        hashmap_s_to_pattern = {}
+        for x in range(len(pattern)):
+            # the case that 
+            if pattern[x] not in hashmap_pattern_to_s and words[x] not in hashmap_s_to_pattern:
+                hashmap_pattern_to_s[pattern[x]] = words[x]
+                hashmap_s_to_pattern[words[x]] = pattern[x]
+            elif (hashmap_pattern_to_s.get(pattern[x]) != words[x]) or (hashmap_s_to_pattern.get(words[x]) != pattern[x]):
+                return False
+        
+        return True
