c4ff59316278c9086bd6b849ce7214df5e8bfe96
Shulin Ji <shulin.ji@mail.utoronto.ca>
2025-03-25 20:44:29 -0400

    added the 3sum closest problem

--- Array_or_String/Two Pointer/16. 3Sum Closest.py
+++ Array_or_String/Two Pointer/16. 3Sum Closest.py
@@ -14,5 +14,36 @@
 # Output: 0
 # Explanation: The sum that is closest to the target is 0. (0 + 0 + 0 = 0).
 
+
+# O(n^2)
 class Solution:
     def threeSumClosest(self, nums: List[int], target: int) -> int:
+        returnSum = None
+        closestDiff = float("inf")
+
+        # Two pointers need to be sorted!
+        nums = sorted(nums)
+
+        #  first is find a number and do a two sum using two pointers
+        for x in range(len(nums)):
+            # find a number and skip the duplicates
+            if x > 0 and nums[x] == nums[x - 1]:
+                continue
+            i = x + 1
+            j = len(nums) - 1
+
+            # Use two pointers to find the smallest diff for current combinations
+            while i < j:
+                currentSum = nums[x] + nums[i] + nums[j]
+                diff = abs(currentSum - target)
+                if diff < closestDiff:
+                    closestDiff = diff
+                    returnSum = currentSum
+                if currentSum < target:
+                    i += 1  # Increase the sum by moving i to the right
+                elif currentSum > target:
+                    j -= 1  # Decrease the sum by moving j to the left
+                else:
+                    return returnSum  # If exactly equal to target, return immediately
+
+        return returnSum
