9ca35b83c5522aa0ef61b2f66c3ec1d9ac0a6fc8
Shulin Ji <shulin.ji@mail.utoronto.ca>
2025-04-04 19:18:36 -0400

    added solution for 658

--- Binary Search/658. Find K Closest Elements.py
+++ Binary Search/658. Find K Closest Elements.py
@@ -27,3 +27,44 @@
 # arr is sorted in ascending order.
 # -104 <= arr[i], x <= 104
 
+
+
+class Solution:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+
+        # Time complexity: O(log(N)+k).
+        # Space complexity: O(1)
+        low, high = 0, len(arr)
+        while low < high:
+            mid = (low + high) // 2
+            if arr[mid] <= x:
+                low = mid + 1
+            else:
+                high = mid
+        
+        # low will be the exact index if it exists 
+        # or will be the index that is the first number bigger than x
+        
+
+        # not contain the edge of the sliding window
+        left, right = low - 1, low
+        while right - left - 1 < k:
+            # if out of bound, only move right side then
+            if left == -1:
+                right += 1
+                continue
+            
+            # if right side is out of bound, only move left bound
+            if right == len(arr):
+                left -= 1
+                continue
+            
+            # compare the abs distance of left and right to x
+            if abs(arr[left] - x) > abs(arr[right] - x):
+                right += 1
+            else:
+                left -= 1
+            
+        # not contain both edge, left + 1 and default right (right is not included)
+        return arr[left + 1:right]
+
