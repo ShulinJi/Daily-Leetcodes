adc82a5bae5738a870c7c10e28ade01b867929c1
Shulin Ji <shulin.ji@mail.utoronto.ca>
2025-04-05 23:06:40 -0400

    added solution for 81

--- Binary Search/81. Search in Rotated Sorted Array II.py
+++ Binary Search/81. Search in Rotated Sorted Array II.py
@@ -40,15 +40,45 @@ class Solution:
             mid = (start + end) // 2
             if nums[mimd] == target:
                 return True
+            
+            # check if binary search could apply in this case
             binary_search_helpful = self.binarySearchHelpful(nums, start, nums[mid])
+            # binary search does not work, we move start to the right, safe move even though not efficient
             if not binary_search_helpful:
                 start += 1
+                continue
 
+            # if mid is in the first half of sorted array
             mid_in_first_half = self.inFirstHalf(nums, start, nums[mid])
+            # if target is in the first half of sorted array
             target_in_first_half = self.inFirstHalf(nums, start, target)
 
+            # ^ operator XOR: only true when only one of them is true
+            # it means mid and target will be at different arrays since if mid is true => in first array, then to satisfy condition,
+            # target needs to be at second sorted array
+            if mid_in_first_half ^ target_in_first_half:
+                # if mid is in first half, then target is in second half, which means we can safely prune the first half
+                if mid_in_first_half:
+                    # prune the first half
+                    start = mid + 1
+                else:
+                    # prune the second half
+                    end = mid - 1
+            # if on the same side of the array, we do the normal binary search because the comparison ums[mid] < target is valid in sorted order!
+            # then the other half that don't have target and mid will be discarded due to binary search
+            else:
+                if nums[mid] < target:
+                    start = mid + 1
+                else:
+                    end = mid - 1
+        return False
+
+
+    # used to check if the number is in the first half of the sorted array
     def inFirstHalf(self, nums, start, element)
         return nums[start] <= element
     
+    # check if binary search is helpful
+    # if nums[start] == nums[mid], we cannot use binary search because it could be not sorted, then binary search would break
     def binarySearchHelpful(self, nums, start, element):
         return nums[start] != element
