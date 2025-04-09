3644dacf949c51d3b2c8ccdfbc459556b0eaa439
Shulin Ji <shulin.ji@mail.utoronto.ca>
2025-04-09 16:24:05 -0400

    added clever way to find patterns

--- Dynamic Programming/1137. N-th Tribonacci Number.py
+++ Dynamic Programming/1137. N-th Tribonacci Number.py
@@ -60,6 +60,7 @@ class Solution:
 
 
 # correct top-down, recursion with memorization
+# O(n) and O(n)
 class Solution:
     def tribonacci(self, n: int) -> int:
         mem = {}
@@ -78,5 +79,16 @@ class Solution:
             return ans
 
         return find_tribonacci(n)
+
+
+# even better, find the pattern and O(n) time, O(1) space complexity
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        if n < 3:
+            return 1 if n else 0
+        a, b, c = 0, 1, 1
+        for _ in range(n - 2):
+            a, b, c = b, c, a + b + c
+        return c
 
         
