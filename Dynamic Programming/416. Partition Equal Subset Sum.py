364851419ffc6e66a95c32909327315f47e5a6b8
Shulin Ji <shulin.ji@mail.utoronto.ca>
2025-04-10 18:40:47 -0400

    added comments

--- Dynamic Programming/416. Partition Equal Subset Sum.py
+++ Dynamic Programming/416. Partition Equal Subset Sum.py
@@ -20,27 +20,39 @@
 # 1 <= nums[i] <= 100
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
+        # memorize for the calculated result, key = (index, accumulated_sum)
         mem = {}
         def find_subset(n, current_sum):
+            # if current_accumulated sum is target_sum, then we find the answer, return True
             if current_sum == target_sum:
                 return True
             
+            # if we reach the end of the array and still couln't find answer, then false
+            # if current_sum is bigger than target_sum, then we didn't find the answer and since the array doesn't have negative number
             if n == -1 or current_sum > target_sum:
                 return False
             
+            # if key is in the mem, directly return
             key = (n, current_sum)
             if key in mem:
                 return mem[key]
+
+            # then we try either: add the current number to the sum, or continue search without adding current number find_subset(n - 1, current_sum)
             result = find_subset(n - 1, current_sum + nums[n]) or find_subset(n - 1, current_sum)
             mem[key] = result
             
             return result
 
+        # if the total sum cannot be divided by 2, then we cannot find a subset that sums up to total_sum // 2
         total_sum = sum(nums)
         if total_sum % 2:
             return False
+
+        # we try to find a subset that can sum up to total_sum // 2, then the rest is also total_sum // 2, then we subsets are equal
         target_sum = total_sum // 2
 
+        # find the anwser from bottom
+        # could also do from the beginning until the end ,doesn't matter
         return find_subset(len(nums) - 1, 0)
 
         
