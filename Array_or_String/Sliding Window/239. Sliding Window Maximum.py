a848c47d4d469fb061ff257a661c07d88274511f
Shulin Ji <shulin.ji@mail.utoronto.ca>
2025-04-25 18:59:27 -0400

    added comments

--- Array_or_String/Sliding Window/239. Sliding Window Maximum.py
+++ Array_or_String/Sliding Window/239. Sliding Window Maximum.py
@@ -32,21 +32,31 @@
 
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        # deque used to track the max in the current window (dq[0] is max), it is decreasing
         dq = deque()
         res = []
 
         for i in range(k):
+            # if current number is larger than the last of the queue, it means we fonud a number that is larger in index and larger in magnitude
+            # then it means that the previous smaller numbers are not in the considerations for the potential max number, we could simply delete it.
             while dq and nums[i] >= nums[dq[-1]]:
+                # delete the smaller and older number
                 dq.pop()
             dq.append(i)
+        # after initial setup of the window, append first answer
         res.append(nums[dq[0]])
 
         for i in range(k, len(nums)):
+            # starting at new window
+            # if current max is out of the window, since i represents the right boundary of the window(current number), i -k + 1represents the left bound
+            # then if dq[0] <= i - k, it means it is out of window and should not be considered
             if dq and dq[0] <= i - k:
                 dq.popleft()
+            # then we do the same stuff for the rest
             while dq and nums[i] >= nums[dq[-1]]:
                 dq.pop()
             dq.append(i)
+            # after everything, we append the current number, and add the max for current window
             res.append(nums[dq[0]])
         
         return res
