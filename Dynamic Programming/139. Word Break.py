e1775ca0eb99901926813fc458a17e47f760b7e9
Shulin Ji <shulin.ji@mail.utoronto.ca>
2025-04-30 15:55:42 -0400

    added new solutions

--- Dynamic Programming/139. Word Break.py
+++ Dynamic Programming/139. Word Break.py
@@ -28,3 +28,19 @@
 # 1 <= wordDict[i].length <= 20
 # s and wordDict[i] consist of only lowercase English letters.
 # All the strings of wordDict are unique.
+
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        dp = [False] * len(s)
+        
+        for i in range(len(s)):
+            for word in wordDict:
+                if i < len(word) - 1:
+                    continue
+                if dp[i - len(word)] or i == len(word) - 1:
+                    if s[i - len(word) + 1: i + 1] == word:
+                        dp[i] = True
+                        break
+                        
+        return dp[-1]
