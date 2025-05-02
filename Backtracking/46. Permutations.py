537f25432609a2c643b714ff6a232f5eebae9806
Shulin Ji <shulin.ji@mail.utoronto.ca>
2025-05-01 23:13:31 -0400

    added solution

--- Backtracking/46. Permutations.py
+++ Backtracking/46. Permutations.py
@@ -21,3 +21,21 @@
 # 1 <= nums.length <= 6
 # -10 <= nums[i] <= 10
 # All the integers of nums are unique.
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        ans = []
+        def backtrack(curr):
+
+            if len(curr) == len(nums):
+                ans.append(list(curr))
+                return 
+            
+            for num in nums:
+                if num not in curr:
+                    curr.append(num)
+                    backtrack(curr)
+                    curr.pop()
+                
+        backtrack([])
+        return ans
