b73be610f834152d6882eede716eea77e4c1dc5e
Shulin Ji <shulin.ji@mail.utoronto.ca>
2025-05-02 20:37:29 -0400

    added optimal solution for backtrack

--- Backtracking/22. Generate Parentheses.py
+++ Backtracking/22. Generate Parentheses.py
@@ -48,3 +48,27 @@ class Solution:
             
         backtrack([])
         return ans
+
+# more optimal solution that eliminates invalid string check
+# Time complexity (4^n / sqrt(n)), space complecity O(n)
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        answer = []
+
+        def backtracking(cur_string, left_count, right_count):
+            if len(cur_string) == 2 * n:
+                answer.append("".join(cur_string))
+                return
+            # if left_count is < n, it means we could still add "(" to the string
+            if left_count < n:
+                cur_string.append("(")
+                backtracking(cur_string, left_count + 1, right_count)
+                cur_string.pop()
+            # if right is < left, it means we could add ")" to our string
+            if right_count < left_count:
+                cur_string.append(")")
+                backtracking(cur_string, left_count, right_count + 1)
+                cur_string.pop()
+
+        backtracking([], 0, 0)
+        return answer
