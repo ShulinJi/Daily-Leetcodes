f2102a2e2851b345bfa2c3ff84ee3086ca7f6748
Shulin Ji <shulin.ji@mail.utoronto.ca>
2025-05-04 19:01:29 -0400

    added solution

--- Binary Search/162. Find Peak Element.py
+++ Binary Search/162. Find Peak Element.py
@@ -25,3 +25,17 @@
 # 1 <= nums.length <= 1000
 # -231 <= nums[i] <= 231 - 1
 # nums[i] != nums[i + 1] for all valid i.
+
+
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        left = 0
+        right = len(nums) - 1
+        while left < right:
+            mid = (left + right) // 2
+            if nums[mid] > nums[mid + 1]:
+                right = mid
+            else:
+                left = mid + 1
+        
+        return left
