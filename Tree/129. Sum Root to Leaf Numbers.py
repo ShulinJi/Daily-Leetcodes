205b8b6e00a4b877273847e0523e6cf33bb9a819
Shulin Ji <shulin.ji@mail.utoronto.ca>
2025-05-26 22:14:00 -0400

    added new approach

--- Tree/129. Sum Root to Leaf Numbers.py
+++ Tree/129. Sum Root to Leaf Numbers.py
@@ -44,7 +44,7 @@
 #         self.right = right
 
 
-# Iterative O(n) and O(n)
+# Iterative O(n) and O(H), H is the height
 class Solution:
     def sumNumbers(self, root: Optional[TreeNode]) -> int:
         total = 0
@@ -61,3 +61,22 @@ class Solution:
                     stack.append((root.right, curr_number))
         
         return total
+
+
+# recursive O(n) and O(H)
+class Solution:
+    def sumNumbers(self, root: TreeNode) -> int:
+        def preorder(r: TreeNode, curr_number: int) -> None:
+            nonlocal root_to_leaf
+            if r:
+                curr_number = curr_number * 10 + r.val
+                # if it's a leaf, update root-to-leaf sum
+                if not (r.left or r.right):
+                    root_to_leaf += curr_number
+
+                preorder(r.left, curr_number)
+                preorder(r.right, curr_number)
+
+        root_to_leaf = 0
+        preorder(root, 0)
+        return root_to_leaf
