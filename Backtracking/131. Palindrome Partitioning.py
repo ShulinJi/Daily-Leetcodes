0ea668a9a23813d6a1bcfabbbddd6c816254e241
Shulin Ji <shulin.ji@mail.utoronto.ca>
2025-07-23 00:26:50 -0400

    added new comments

--- Backtracking/131. Palindrome Partitioning.py
+++ Backtracking/131. Palindrome Partitioning.py
@@ -28,11 +28,14 @@ class Solution:
         return s == s[::-1]
 
     def dfs(self, s: str, path: List[str], result: List[List[str]]):
+        # Base case which we have exhausted all the remaining string, means all the components are palindrome
         if not s:
             result.append(path)
             return
+        
         for i in range(1, len(s) + 1):
+            # we don't add a break if it's not palindrome because we want to check longer substrings. ex. ab is not, but aba is
             if self.isPalindrome(s[:i]):
-                # add current substring in the currentList
+                # add current substring in the currentList, s[i:] is the rest of the string, don't need to remove element from path since it is just a copy
                 self.dfs(s[i:], path + [s[:i]], result)
                 # backtrack and remove the current substring from currentList
