35a3a8f9df1ae52c6a2ee077dee5da9d7c6a5b6a
Shulin Ji <shulin.ji@mail.utoronto.ca>
2025-08-08 18:43:03 -0400

    added new approach

--- Binary Search/852. Peak Index in a Mountain Array.py
+++ Binary Search/852. Peak Index in a Mountain Array.py
@@ -31,3 +31,21 @@
 # 3 <= arr.length <= 105
 # 0 <= arr[i] <= 106
 # arr is guaranteed to be a mountain array.
+
+# O(log(n)) solution using binary search
+class Solution:
+    def peakIndexInMountainArray(self, arr: List[int]) -> int:
+        left = 0
+        right = len(arr) - 1
+
+        while left <= right:
+            mid = left + (right - left) // 2
+            if arr[mid] > arr[mid + 1] and arr[mid] > arr[mid - 1] :
+                return mid
+                # on the rise
+            elif arr[mid - 1] < arr[mid] < arr[mid + 1]:
+                left = mid + 1
+            else:
+                # downhill
+                right = mid 
+        
