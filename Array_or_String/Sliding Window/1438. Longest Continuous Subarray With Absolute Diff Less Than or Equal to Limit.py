012e0d9fa9e4c1f1e97c42d5b364b9aab9a1c6fd
Shulin Ji <shulin.ji@mail.utoronto.ca>
2025-08-13 21:37:56 -0400

    added better solution

--- Array_or_String/Sliding Window/1438. Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit.py
+++ Array_or_String/Sliding Window/1438. Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit.py
@@ -35,6 +35,39 @@
 # 1 <= nums[i] <= 109
 # 0 <= limit <= 109
 
+# O(n) time, O(n) space, sliding window solution, using deques to maintain the max and min values in the current window
+class Solution:
+    def longestSubarray(self, nums: List[int], limit: int) -> int:
+        max_deque = deque()
+        min_deque = deque()
+        left = 0
+        max_length = 0
+
+        for right in range(len(nums)):
+            # Maintain the max_deque in decreasing order, the first element is the maximum in the current window and oldest element
+            while max_deque and max_deque[-1] < nums[right]:
+                max_deque.pop()
+            max_deque.append(nums[right])
+
+            # Maintain the min_deque in increasing order
+            while min_deque and min_deque[-1] > nums[right]:
+                min_deque.pop()
+            min_deque.append(nums[right])
+
+            # Check if the current window exceeds the limit
+            while max_deque[0] - min_deque[0] > limit:
+                # Remove the elements that are out of the current window, if they match, just pop them, if not, move the left pointer
+                # and eventaully pop the leftmost elements from the deques
+                if max_deque[0] == nums[left]:
+                    max_deque.popleft()
+                if min_deque[0] == nums[left]:
+                    min_deque.popleft()
+                left += 1
+
+            max_length = max(max_length, right - left + 1)
+
+        return max_length
+
 # O(n log n) time, O(n) space, sliding window with heaps solution
 class Solution:
     def longestSubarray(self, nums: List[int], limit: int) -> int:
