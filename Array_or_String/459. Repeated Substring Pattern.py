36eb58c60734c4103f1935efba17718041f8e689
Shulin Ji <shulin.ji@mail.utoronto.ca>
2025-08-17 21:37:22 -0400

    added new comments

--- Array_or_String/459. Repeated Substring Pattern.py
+++ Array_or_String/459. Repeated Substring Pattern.py
@@ -23,12 +23,15 @@
 # 1 <= s.length <= 104
 # s consists of lowercase English letters.
 
-
+# O(n * sqrt(n)) time | O(1) space
 class Solution:
     def repeatedSubstringPattern(self, s: str) -> bool:
         n = len(s)
+        # check for all possible lengths of substring from 1 to n/2, n// 2 + 1 because a substring can't be longer than n/2 (in order to repeat at least twice)
         for i in range(1, n // 2 + 1):
+            # if n is divisible by i, then we can form a new string by repeating the substring of length i, n // i times
             if n % i == 0:
+                # form the new string by repeating the substring of length i, n // i times
                 new_string = s[:i] * (n // i)
                 if new_string == s:
                     return True
