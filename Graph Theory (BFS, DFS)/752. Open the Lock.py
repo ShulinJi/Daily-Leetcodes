707e828a86599061011fc63dbf29cf01ef4581fa
Shulin Ji <shulin.ji@mail.utoronto.ca>
2025-08-20 14:41:20 -0400

    added new approach

--- Graph Theory (BFS, DFS)/752. Open the Lock.py
+++ Graph Theory (BFS, DFS)/752. Open the Lock.py
@@ -35,3 +35,76 @@
 # target.length == 4
 # target will not be in the list deadends.
 # target and deadends[i] consist of digits only.
+
+
+# O(4(d + 10^4)) time | O(4(d + 10^4)) space with BFS
+class Solution:
+    def openLock(self, deadends: List[str], target: str) -> int:
+        # d is the number of deadends, 10^4 is the number of all possible combinations of the lock
+        # next_slot and prev_slot are used to generate new combinations of the lock. Next slot is the next number in the wheel, and prev slot is the previous number in the wheel.
+        next_slot = {
+            "0": "1",
+            "1": "2",
+            "2": "3",
+            "3": "4",
+            "4": "5",
+            "5": "6",
+            "6": "7",
+            "7": "8",
+            "8": "9",
+            "9": "0",
+        }
+        prev_slot = {
+            "0": "9",
+            "1": "0",
+            "2": "1",
+            "3": "2",
+            "4": "3",
+            "5": "4",
+            "6": "5",
+            "7": "6",
+            "8": "7",
+            "9": "8",
+        }
+
+        # visited_combination used to store all the combinations that have been visited, so we don't visit them again
+        visited_combination = set(deadends)
+        pending_combination = deque()
+
+        if "0000" in visited_combination:
+            return -1 
+
+        turn = 0
+        pending_combination.append("0000")
+        while pending_combination:
+            # current_level_count is the number of combinations in the current level of the BFS tree
+            current_level_count = len(pending_combination)
+            for _ in range(current_level_count):
+                current_combination = pending_combination.popleft()
+                if current_combination == target:
+                    return turn
+                # generate new comb with next_slot and prev_slot, each with 4 wheels, so we have 4 * 2 = 8 new combinations
+                for i in range(4):
+                    new_list = list(current_combination)
+                    # new combination with next slot
+                    new_list[i] = next_slot[new_list[i]]
+                    new_combination = "".join(new_list)
+                    if new_combination not in visited_combination:
+                        pending_combination.append(new_combination)
+                        # add the new combination to the visited_combination set in the same time to avoid duplicates
+                        visited_combination.add(new_combination)
+                    
+                    new_list = list(current_combination)
+                    # new combination with previous slot
+                    new_list[i] = prev_slot[new_list[i]]
+                    new_combination = "".join(new_list)
+                    if new_combination not in visited_combination:
+                        pending_combination.append(new_combination)
+                        visited_combination.add(new_combination)
+
+            turn += 1
+
+        return -1
+        
+
+
