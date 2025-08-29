6fd2a4968816968b557e6137be5a72d2e85492b1
Shulin Ji <shulin.ji@mail.utoronto.ca>
2025-08-28 23:47:56 -0400

    added new approach

--- Dynamic Programming/10. Regular Expression Matching.py
+++ Dynamic Programming/10. Regular Expression Matching.py
@@ -31,16 +31,20 @@
 # p contains only lowercase English letters, '.', and '*'.
 # It is guaranteed for each appearance of the character '*', there will be a previous valid character to match.
 
+# time complexity: O((T+P) * 2 ^(T + P)) where S is the length of string and T is the length of pattern
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
         # if we exhausted the pattern but the text is still not empty, then not match false
         if not p:
             return not s
         
+        # first char matches if s is not empty and the first char of p is either '.' or matches the first char of s
+        # counter-example: s = "a", p = "b", without checking first char of s, it would return True
         first_match = bool(s) and p[0] in {s[0], '.'}
 
         if len(p) >= 2 and p[1] == "*":
             # 2 cases, first is to skip the pattern a*, or we keep matching aaaa with a* (consume)
             return self.isMatch(s, p[2:]) or first_match and self.isMatch(s[1:], p)
         else:
+            # move to next char in both s and p if first char matches
             return first_match and self.isMatch(s[1:], p[1:])
