fed4ff897c3f24b6d5ad0e8a7e72f7648c8c7759
Shulin Ji <shulin.ji@mail.utoronto.ca>
2025-08-29 00:08:57 -0400

    added new bottom-up approach

--- Dynamic Programming/10. Regular Expression Matching.py
+++ Dynamic Programming/10. Regular Expression Matching.py
@@ -59,6 +59,23 @@ class Solution:
             return memo[i, j]
 
         return dp(0, 0)
+
+# Bottom-up DP
+# time complexity: O(S * P) where S is the length of string and P is
+class Solution(object):
+    def isMatch(self, text: str, pattern: str) -> bool:
+        dp = [[False] * (len(pattern) + 1) for _ in range(len(text) + 1)]
+
+        dp[-1][-1] = True
+        for i in range(len(text), -1, -1):
+            for j in range(len(pattern) - 1, -1, -1):
+                first_match = i < len(text) and pattern[j] in {text[i], "."}
+                if j + 1 < len(pattern) and pattern[j + 1] == "*":
+                    dp[i][j] = dp[i][j + 2] or first_match and dp[i + 1][j]
+                else:
+                    dp[i][j] = first_match and dp[i + 1][j + 1]
+
+        return dp[0][0]
     
 # TLE 
 # time complexity: O((T+P) * 2 ^(T + P)) where S is the length of string and T is the length of pattern
