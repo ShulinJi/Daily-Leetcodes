5b78d1e26381c8418e4878f641ea19bd325da878
Shulin Ji <shulin.ji@mail.utoronto.ca>
2025-09-02 21:19:04 -0400

    added new solutions

--- Matrix/37. Sudoku Solver.py
+++ Matrix/37. Sudoku Solver.py
@@ -26,3 +26,74 @@
 # board[i][j] is a digit or '.'.
 # It is guaranteed that the input board has only one solution.
 
+class Solution:
+    def solveSudoku(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        n = 3
+        N = n * n
+
+        # use dictionaries to keep track of the counts of 9 (1-9) digits in each row, column, and box
+        rows = [defaultdict(int) for _ in range(N)]
+        cols = [defaultdict(int) for _ in range(N)]
+        boxes = [defaultdict(int) for _ in range(N)]
+
+        # initialize the dictionaries with the given board, fill in the counts of the digits
+        for i in range(N):
+            for j in range(N):
+                if board[i][j] != ".":
+                    d = int(board[i][j])
+                    rows[i][d] += 1
+                    cols[j][d] += 1
+                    box_index = (i // 3) * 3 + j // 3
+                    boxes[box_index][d] += 1
+        
+        def backtracking(row, col):
+            # if we reached the end of the board, we found a solution
+            if row == N:
+                return True
+            
+            # move to the next cell from left to right, top to bottom
+            if col == N - 1:
+                next_row = row + 1
+                next_col = 0
+            else:
+                next_col = col + 1
+                next_row = row
+
+            # if the cell is already filled, move to the next cell, skip this cell
+            if board[row][col] != ".":
+                return backtracking(next_row, next_col)
+            
+            # try each digit from 1 to 9
+            current_box = box_index = (row // 3) * 3 + col // 3
+            for d in range(1, 10):
+                # check if the digit is not already in the current row, column, and box
+                if not (d in rows[row] or d in cols[col] or d in boxes[current_box]):
+                    # place the digit in the cell
+                    board[row][col] = str(d)
+                    rows[row][d] += 1
+                    cols[col][d] += 1
+                    boxes[current_box][d] += 1
+
+                    # move to the next cell and continue the backtracking
+                    if backtracking(next_row, next_col):
+                        return True
+                    
+                    # we only reach here if placing d doesn't lead to a solution, returned a false from backtracking
+                    # if placing d doesn't lead to a solution, remove it and try the next digit
+                    board[row][col] = "."
+                    rows[row][d] -= 1
+                    cols[col][d] -= 1
+                    boxes[current_box][d] -= 1
+
+                    # clean up the dictionaries to remove the count of 0 to save space
+                    # delete ecause when we check if d in rows[row], if count is 0, it still returns True, which is incorrect
+                    if rows[row][d] == 0: del rows[row][d]
+                    if cols[col][d] == 0: del cols[col][d]
+                    if boxes[current_box][d] == 0: del boxes[current_box][d]
+
+            return False
+        
+        backtracking(0, 0)
