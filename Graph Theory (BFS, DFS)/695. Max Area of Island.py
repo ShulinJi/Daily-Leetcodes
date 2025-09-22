9ec0c90d963ff352c6d777c96cbb3ae51f6ccd2d
Shulin Ji <shulin.ji@mail.utoronto.ca>
2025-09-22 17:14:47 -0400

    added new solution DFS

--- Graph Theory (BFS, DFS)/695. Max Area of Island.py
+++ Graph Theory (BFS, DFS)/695. Max Area of Island.py
@@ -24,3 +24,35 @@
 # n == grid[i].length
 # 1 <= m, n <= 50
 # grid[i][j] is either 0 or 1.
+
+# O(m * n) time complexity
+# O(m * n) space complexity
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        m = len(grid)
+        n = len(grid[0])
+        max_area = 0
+        # set to store visited cells
+        seen = set()
+
+        def dfs(r, c):
+            # base case if out of bound or already visited
+            if (r < 0 or r >= m or c < 0 or c >= n) or (r, c) in seen:
+                return 0
+            # base case if it is not 1
+            if grid[r][c] == 0:
+                return 0
+            # mark as visited
+            seen.add((r, c))
+
+            # explore all 4 directions, up, down, left, right
+            return 1 + dfs(r + 1, c) + dfs(r - 1, c) + dfs(r, c + 1) + dfs(r, c - 1)
+
+        for r in range(m):
+            for c in range(n):
+                # if it is 1 and not visited
+                if grid[r][c] == 1:
+                    area = dfs(r, c)
+                    max_area = max(area, max_area)
+        
+        return max_area
