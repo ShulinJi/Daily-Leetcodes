45649caf8427c29b7a7676439b748b7c582a5d55
Shulin Ji <shulin.ji@mail.utoronto.ca>
2025-09-22 12:56:31 -0400

    added new solution

--- Graph Theory (BFS, DFS)/827. Making A Large Island.py
+++ Graph Theory (BFS, DFS)/827. Making A Large Island.py
@@ -29,3 +29,33 @@
 # n == grid[i].length
 # 1 <= n <= 500
 # grid[i][j] is either 0 or 1.
+
+# O(n ^ 4) time complexity, which exceeds the time limit
+class Solution:
+    def largestIsland(self, grid: List[List[int]]) -> int:
+        n = len(grid)
+        max_area = 0
+        seen = set()
+
+        def dfs(r, c):
+            if (r < 0 or r > n - 1 or c < 0 or c > n - 1) or (r, c) in seen:
+                return 0
+            if grid[r][c] == 0:
+                return 0
+            seen.add((r, c))
+
+            return 1 + dfs(r - 1, c) + dfs(r + 1, c) + dfs(r, c - 1) + dfs(r, c + 1)
+
+
+        for r in range(n):
+            for c in range(n):
+                if grid[r][c] == 0:
+                    grid[r][c] = 1
+                    max_area = max(dfs(r, c), max_area)
+                    grid[r][c] = 0
+                    seen.clear()
+                else:
+                    max_area = max(dfs(r, c), max_area)
+                    seen.clear()
+
+        return max_area
