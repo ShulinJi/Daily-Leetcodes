fa5a242d32d6152d1c861bc0339fb582ef4463a1
Shulin Ji <shulin.ji@mail.utoronto.ca>
2025-09-29 11:00:59 -0400

    added new comments

--- stack/224. Basic Calculator.py
+++ stack/224. Basic Calculator.py
@@ -31,11 +31,16 @@
 class Solution:
     def evaluate(self, stack):
         res = 0
+        # if the stack is empty or the top element is an operator, we push 0 to handle cases like "-2+1" or "(-2+1)"
+        # so that we can pop 0 and apply the operator correctly
         if not stack or type(stack[-1]) == str:
             stack.append(0)
+        # first element will always be a number
         res = stack.pop()
         while stack and stack[-1] != ")":
+            # next element will always be an operator
             sign = stack.pop()
+            # pop the number and apply the operator
             if sign == "+":
                 res += stack.pop()
             else:
@@ -45,15 +50,22 @@ class Solution:
     def calculate(self, s: str) -> int:
         stack = []
         n, operand = 0, 0
-
+        # we traverse the string in reverse order so that we can easily handle - as a unary operator
+        # because we are pushing to stack, if we don't reverse, the order poped from stack for - will be in correct order
+        # example: "1 - 2 + 3", if we traverse from left to right, stack will be [1, '-', 2, '+', 3], when we pop, we will get 3 first, then +, then 2, then -, then 1
+        # which will be evaluated as 3 + 2 - 1 = 4, which is incorrect
         for i in range(len(s) - 1, -1, -1):
+            # if the character is a digit, we keep building the operand in reverse order
             if s[i].isdigit():
                 operand = (10 ** n * int(s[i])) + operand
                 n += 1
             elif s[i] != " ":
+                # if n is not 0, we have an operand waiting to push to stack and reset n and operand for next operand
                 if n:
                     stack.append(operand)
                     n, operand = 0, 0
+                #  if the character is an opening bracket "(", we evaluate the expression till we find the closing bracket
+                # else we just push the operator or closing bracket to stack "+ - )"
                 if s[i] == "(":
                     res = self.evaluate(stack)
                     stack.pop()
