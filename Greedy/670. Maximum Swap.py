874a1fee1f44b1c7e04166416f5121e403953d76
Shulin Ji <shulin.ji@mail.utoronto.ca>
2025-09-28 23:08:47 -0400

    added new solution

--- Greedy/670. Maximum Swap.py
+++ Greedy/670. Maximum Swap.py
@@ -20,6 +20,28 @@
 
 # 0 <= num <= 108
 
+# O(n) time | O(n) space
+# Greedy
+class Solution:
+    def maximumSwap(self, num: int) -> int:
+        num_list = list(str(num))
+        n = len(num_list)
+        max_right_index = [0] * n
+        max_right_index[n - 1] = n - 1
+
+        # One pass to fill the max_right_index array
+        for i in range(n - 2, -1, -1):
+            max_right_index[i] = i if num_list[i] > num_list[max_right_index[i + 1]] else max_right_index[i + 1]
+        
+        # if we find a digit that is less than the max digit on its right, we swap and return the result
+        # because earliest such swap will yield the maximum result
+        for i in range(n):
+            if num_list[i] < num_list[max_right_index[i]]:
+                num_list[i], num_list[max_right_index[i]] = num_list[max_right_index[i]], num_list[i]
+                return int("".join(num_list))
+        
+        return num
+
 # brute force
 # O(n^2) time | O(n) space
 class Solution:
