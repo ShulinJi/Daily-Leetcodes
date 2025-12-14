d865d919192629bf286fcd46bcd81c3b6eccaca6
Shulin Ji <shulin.ji@mail.utoronto.ca>
2025-12-13 19:57:42 -0500

    added new slution

--- Array_or_String/Fast and slow pointers/234. Palindrome Linked List.py
+++ Array_or_String/Fast and slow pointers/234. Palindrome Linked List.py
@@ -39,4 +39,47 @@ class Solution:
         
         return nodes == nodes[::-1]
 
+
+# O(n) and O(1) solution
+# flip the second half of the linked list and compare with the first half to see if they are the same
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        def reverse_link(self, head):
+            prev = None
+            curr = head
+            while curr:
+                next_node = curr.next
+                curr.next = prev
+                prev = curr
+                curr = next_node
+            return prev
         
+        def find_the_first_half(self, head):
+            fast = head
+            slow = head
+            while fast.next and fast.next.next:
+                slow = slow.next
+                fast = fast.next.next
+            return slow
+
+        if head is None:
+            return True
+
+        # Find the end of first half
+        first_half_end = self.end_of_first_half(head)
+        # flip the second half
+        second_half_start = self.reverse_list(first_half_end.next)
+
+        # Check whether or not there's a palindrome.
+        result = True
+        first_position = head
+        second_position = second_half_start
+        while result and second_position is not None:
+            if first_position.val != second_position.val:
+                result = False
+            first_position = first_position.next
+            second_position = second_position.next
+
+        # Restore the list and return the result.
+        first_half_end.next = self.reverse_list(second_half_start)
+        return result    
