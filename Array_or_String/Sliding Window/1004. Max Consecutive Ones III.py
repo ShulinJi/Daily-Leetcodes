5afa083b66f732e5699e9baa0feac5e327c8ca81
Shulin Ji <shulin.ji@mail.utoronto.ca>
2026-01-02 00:27:25 -0500

    added new solution

--- Array_or_String/Sliding Window/1004. Max Consecutive Ones III.py
+++ Array_or_String/Sliding Window/1004. Max Consecutive Ones III.py
@@ -22,7 +22,26 @@
 # nums[i] is either 0 or 1.
 # 0 <= k <= nums.length
 
+# second attempt
+# O(n) time | O(1) space
+class Solution:
+    def longestOnes(self, nums: List[int], k: int) -> int:
+        left = 0
+        ans = 0
 
+        for right in range(len(nums)):
+            # if we see a 0, then exhaust an available 0
+            if nums[right] == 0:
+                k -= 1
+            
+            # if there is more than k 0's has been used, we need to move left until the window is legal again by freeing up a 0
+            while k < 0:
+                if nums[left] == 0:
+                    k += 1
+                left += 1
+            ans = max(ans, right - left + 1)
+        
+        return ans
 # My own solution by shrinking the window size, runtime still O(n)
 
 
