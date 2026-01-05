a0bf938e4fa3439b84397e8c0ce9e0d50ad560f2
Shulin Ji <shulinji1024@gmail.com>
2026-01-05 16:05:52 -0500

    Refactor isPalindrome method for efficiency
    Refactored palindrome check using two pointers to skip non-alphanumeric characters and compare letters in a case-insensitive manner.

--- Array_or_String/Two Pointer/125. Valid Palindrome.py
+++ Array_or_String/Two Pointer/125. Valid Palindrome.py
@@ -1,3 +1,31 @@
+# second attempt. 
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        left = 0
+        right = len(s) - 1
+        
+        while left < right:
+            # use isalpha() also works since it only says letters
+            # skip all the non-alphbetic letters
+            while not s[left].isalnum() and left < right:
+                left += 1
+            
+            while not s[right].isalnum() and left < right:
+                right -= 1
+            
+            # converting to lower case as required in question
+            if s[left].lower() != s[right].lower():
+                return False
+            
+            left += 1
+            right -= 1
+
+        return True
+
+
+
+
+
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         # stringList = ''.join([x.lower() for x in s if x.isalpha() or x.isdigit()])
