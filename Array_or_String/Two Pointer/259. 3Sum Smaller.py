e075e903f5dc7f8963e56d7993cf73b8d29b4e51
Shulin Ji <shulinji1024@gmail.com>
2026-01-06 14:32:52 -0500

    Add threeSumSmaller function implementation
    Implement threeSumSmaller function to count triplets with sum less than target.

--- Array_or_String/Two Pointer/259. 3Sum Smaller.py
+++ Array_or_String/Two Pointer/259. 3Sum Smaller.py
@@ -14,6 +14,30 @@
 # Example 3:
 # Input: nums = [0], target = 0
 # Output: 0
- 
+
+
 class Solution:
     def threeSumSmaller(self, nums: List[int], target: int) -> int:
+        # O(n^2) time and O(1) space if doesn't count the space for sorting, if count, it will be O(logn)
+        nums.sort()
+        ans = 0
+        for i in range(len(nums)):
+            # first find the complement
+            complement = target - nums[i]
+            left = i + 1
+            right = len(nums) - 1
+            #  takes O(n) to find the satisfied comb
+            while left < right:
+                # if the condition satisfy, it means that there are (right - left) many comb between them that satisfy the
+                # the condition because the array is sorted, if the current right is good, then any right smaller than
+                # current index would also work, it is like we move the right pointer to the left and that's interval 
+                # satisfy the the condition
+                if nums[left] + nums[right] < complement:
+                    ans += right - left
+                    # we only move the left because the current situation is smaller
+                    left += 1
+                else:
+                    # we try to move right pointer to make it smaller to satify the condition
+                    right -= 1
+        
+        return ans
