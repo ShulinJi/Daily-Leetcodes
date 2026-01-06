fd26a3eefe227711e7010643392f471bb493a339
Shulin Ji <shulinji1024@gmail.com>
2026-01-06 16:02:37 -0500

    Add two-pointer solution for valid word abbreviation
    Implemented a two-pointer approach to validate word abbreviation, handling character matches and numeric skips.

--- Array_or_String/Two Pointer/408. Valid Word Abbreviation.py
+++ Array_or_String/Two Pointer/408. Valid Word Abbreviation.py
@@ -38,6 +38,36 @@
 # abbr consists of lowercase English letters and digits.
 # All the integers in abbr will fit in a 32-bit integer.
 
+# SECOND ATTEMPT
+class Solution:
+    def validWordAbbreviation(self, word: str, abbr: str) -> bool:
+        p1 = 0
+        p2 = 0
+
+        while p1 < len(word) and p2 < len(abbr):
+            # if in abbr it is char, then they must match
+            if abbr[p2].isalpha():
+                # if they match, we check the next index of both string
+                if abbr[p2] == word[p1]:
+                    p1 += 1
+                    p2 += 1
+                else:
+                    return False
+            else:
+                # now we are in else, it means we have a number, then no leading zero
+                if abbr[p2] == "0":
+                    return False
+                
+                # we form the number to see how many characters we need to skip
+                num = 0
+                while p2 < len(abbr) and abbr[p2].isdigit():
+                    num = num * 10 + int(abbr[p2])
+                    p2 += 1
+                # we need to skip this many characters for p1
+                p1 += num
+            
+        return p1 == len(word) and p2 == len(abbr)
+
 
 class Solution:
     def validWordAbbreviation(self, word: str, abbr: str) -> bool:
