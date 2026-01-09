cd21829f3e8a7288629d5d5ce80b687b2ba9090a
Shulin Ji <shulinji1024@gmail.com>
2026-01-09 18:53:08 -0500

    Update 179. Largest Number.py

--- Array_or_String/179. Largest Number.py
+++ Array_or_String/179. Largest Number.py
@@ -19,6 +19,22 @@
 # 1 <= nums.length <= 100
 # 0 <= nums[i] <= 109
 
+# SECOND ATTEMPT, O(n)
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        num_string = [str(num) for num in nums]
+
+        # we use x * 10 so that each string is repeated by 10 times, and since in the constraints the number 0 <= nums[i] <= 10^9, then we can cover all the cases, 
+        # By reapeating 10 times, we can compare the true who's big problem: normally, we have 34 > 30 > 3, but now we have 34 > 3 > 30 because 343434... > 3333... > 30303030...
+        # why? because we are considering who comes before who, 34+3 = 343 > 334, so 34 > 3, similar 330 > 303, that's why we order this way, and by normalizing the number using *10 to realize it!!!
+
+        # python uses powersort
+        num_string.sort(key=lambda x: x * 10, reverse=True)
+        # if after sort, we still have 0 at front, then the number is 0
+        if num_string[0] == "0":
+            return "0"
+        
+        return "".join(num_string)
 
 class Solution:
     def largestNumber(self, nums: List[int]) -> str:
