5aabe0d119f240a049ff13a522474e9cad0fd471
Shulin Ji <shulinji1024@gmail.com>
2026-01-11 14:20:08 -0500

    Update 252. Meeting Rooms.py

--- Array_or_String/252. Meeting Rooms.py
+++ Array_or_String/252. Meeting Rooms.py
@@ -1,3 +1,36 @@
+# Given an array of meeting time intervals where intervals[i] = [starti, endi], determine if a person could attend all meetings.
+
+# Example 1:
+
+# Input: intervals = [[0,30],[5,10],[15,20]]
+# Output: false
+# Example 2:
+
+# Input: intervals = [[7,10],[2,4]]
+# Output: true
+ 
+
+# Constraints:
+
+# 0 <= intervals.length <= 104
+# intervals[i].length == 2
+# 0 <= starti < endi <= 106
+
+
+# SECOND ATTEMPT
+class Solution:
+    def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
+        # O(nlogn) and O(n) for python sort
+        intervals.sort()
+
+        for i in range(1, len(intervals)):
+            # after sort based on the start time, if the start of the next interval is smaller than prev end time, then overlap
+            if intervals[i][0] < intervals[i - 1][1]:
+                return False
+        
+        return True
+
+
 class Solution:
     def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
         # Brute force approcach, too slow
