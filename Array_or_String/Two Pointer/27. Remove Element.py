709cbadabc83fc191d242db70b1deb591997d740
Shulin Ji <shulinji1024@gmail.com>
2026-01-11 15:03:37 -0500

    Add removeElement function to handle value removal
    Implement removeElement function to remove all occurrences of a specified value from an array in-place. The function uses a two-pointer approach for optimal performance.

---
+++ Array_or_String/Two Pointer/27. Remove Element.py
@@ -0,0 +1,111 @@
+# Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
+# Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
+# Change the array nums such that the first k elements of nums contain the elements which are not equal to val. The remaining elements of nums are not important as well as the size of nums.
+# Return k.
+# Custom Judge:
+
+# The judge will test your solution with the following code:
+
+# int[] nums = [...]; // Input array
+# int val = ...; // Value to remove
+# int[] expectedNums = [...]; // The expected answer with correct length.
+#                             // It is sorted with no values equaling val.
+
+# int k = removeElement(nums, val); // Calls your implementation
+
+# assert k == expectedNums.length;
+# sort(nums, 0, k); // Sort the first k elements of nums
+# for (int i = 0; i < actualLength; i++) {
+#     assert nums[i] == expectedNums[i];
+# }
+# If all assertions pass, then your solution will be accepted.
+
+# Example 1:
+# Input: nums = [3,2,2,3], val = 3
+# Output: 2, nums = [2,2,_,_]
+# Explanation: Your function should return k = 2, with the first two elements of nums being 2.
+# It does not matter what you leave beyond the returned k (hence they are underscores).
+
+# Example 2:
+# Input: nums = [0,1,2,2,3,0,4,2], val = 2
+# Output: 5, nums = [0,1,4,0,3,_,_,_]
+# Explanation: Your function should return k = 5, with the first five elements of nums containing 0, 0, 1, 3, and 4.
+# Note that the five elements can be returned in any order.
+# It does not matter what you leave beyond the returned k (hence they are underscores).
+ 
+
+# Constraints:
+
+# 0 <= nums.length <= 100
+# 0 <= nums[i] <= 50
+# 0 <= val <= 100
+
+# SECOND ATTEMPT
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        # i is the count of numbers of elements not equal to val
+        i = 0
+        for j in range(len(nums)):
+            # then whenever we meet a number that does not equal to the val
+            # we increment i and rewrite the nums[i]
+            # in this way, we only write to the list with numbers that are not equal to the val
+            if nums[j] != val:
+                nums[i] = nums[j]
+                i += 1
+        return i
+
+# same idea, O(n) and O(1)
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        i = 0
+        j = len(nums) - 1
+
+        while i <= j:
+            if nums[i] == val:
+                # we meet the numbers == val, we swap it with randome at the back since order does not matter
+                nums[i] = nums[j]
+                # we decrease the j and keep the i the same because we are not sure if the number gets swapped back
+                # might also equal to the val
+                j -= 1
+            else:
+                # we have numbers != val, just increment
+                i += 1
+        
+        return i
+
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        ### Two pointer O(N) runtime 
+        i = 0
+        for j in range(len(nums)):
+            # Whenver we meet a value equal to val, we simply don't copy and skip it
+            if nums[j] != val:
+                nums[i] = nums[j]
+                i += 1
+
+        return i
+
+        ### Two pointer with O(N)
+        i = 0
+        n = len(nums)
+        while i < n:
+            # when we meet a value equals to val, we simply swap it with the other elements at the end since order doesn't matter
+            if nums[i] == val:
+                nums[i] = nums[n - 1]
+                n -= 1
+            else:
+            # if it doesn't equal, then we simply proceed
+                i += 1
+        return n
+
+        ### Brute Force,
+        count = 0
+        for x in nums:
+            if x == val:
+                count += 1
+        # O(N^2)
+        for y in range(count):
+            nums.remove(val)
+        
+        return len(nums)
