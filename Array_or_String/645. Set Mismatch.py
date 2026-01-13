30b011d5f03394890ba53778bd128592cf5e91ca
Shulin Ji <shulinji1024@gmail.com>
2026-01-13 17:41:06 -0500

    Update 645. Set Mismatch.py

--- Array_or_String/645. Set Mismatch.py
+++ Array_or_String/645. Set Mismatch.py
@@ -21,6 +21,56 @@
 # 2 <= nums.length <= 104
 # 1 <= nums[i] <= 104
 
+# SECOND ATTEMPT, O(n) and O(1)
+class Solution:
+    def findErrorNums(self, nums: List[int]) -> List[int]:
+        # O(n) and O(1) constant!
+        duplicate = 0
+        missing = 0
+
+        # we invert all the indices in the array, then if there is a duplicate, we will find it being negative!
+        for num in nums:
+            # we need to -1 because the actual value is from 1 - n, not from 0 - (n-1)
+            if nums[abs(num) - 1] < 0:
+                duplicate = abs(num)
+            else:
+                nums[abs(num) - 1] *= -1
+        
+        # then after the inversion, the only one that is positive is the one that is missing, which is positive number index + 1
+        # example, [1,2,2,3,5], after inversion [-1, -2, -2, 3, -5], 3 is positive because we miss the number 4!
+        for i in range(len(nums)):
+            if nums[i] > 0:
+                missing = i + 1
+                break
+        
+        return [duplicate, missing]
+
+
+# O(n) and O(n) hashmap, can only use array instead of the hashmap where each element is the number of occurance of that number, if arr[i] is 0, then the missing number is the i
+from collections import Counter
+class Solution:
+    def findErrorNums(self, nums: List[int]) -> List[int]:
+        num_map = Counter(nums)
+        dup = 0
+        missing = 0
+
+        # loop from 1 - n
+        for num in range(1, len(nums) + 1):
+            # brek early if we have found the answer
+            if dup and missing:
+                break
+            # if not in the map
+            if num not in num_map:
+                missing = num
+            # if more than 1 occurrance
+            if num_map[num] > 1:
+                dup = num
+        
+        return [dup, missing]
+
+
+
+
 # O(n) time and O(1) space
 class Solution:
     def findErrorNums(self, nums: List[int]) -> List[int]:
