ea6b1a7ca888c1360e65d0cf70d1937a7f8f6f26
Shulin Ji <shulinji1024@gmail.com>
2026-01-14 17:38:27 -0500

    Update 68. Text Justification.py

--- Array_or_String/68. Text Justification.py
+++ Array_or_String/68. Text Justification.py
@@ -55,6 +55,63 @@
 # 1 <= maxWidth <= 100
 # words[i].length <= maxWidth
 
+# SECOND ATTEMPT
+class Solution:
+    def fullJustify(self, words: List[str], maxWidth: int) -> List[str]:
+        # first we have a subfunction to form a line
+        def find_word(i):
+            current_length = 0
+            current_line = []
+            # current_length + len(words[i]) <= maxWidth, no +1 bc last words can without whitepace
+            while i < len(words) and current_length + len(words[i]) <= maxWidth:
+                current_line.append(words[i])
+                # we use +1 bc the each word needs to followed by a whitespace
+                current_length += len(words[i]) + 1
+                i += 1
+            
+            return current_line
+        
+        # another function to padding the whiteaspace to current line
+        def form_line(i, line):
+            # we start with -1 because the last word can leave without white space
+            base_length = -1
+            for word in line:
+                # defalt length with one extra space
+                base_length += len(word) + 1
+            
+            # how may extra spaces we have that needs to take care (whitespaces to add)
+            extra_space = maxWidth - base_length
+            # if we are dealing with last line, it is left-justified
+            if len(line) == 1 or i == len(words):
+                return " ".join(line) + " " * extra_space
+
+            # -1 because we are ignoring the last word, don't need white space
+            words_need_padding = len(line) - 1
+            # those many whitespaces that needs to pad for each word except last one
+            padding_space = extra_space // words_need_padding
+            # cannot distribute evenly, need to padd extra ones starting from left
+            extra_uneven_padding = extra_space % words_need_padding
+
+            # pad those uneven ones first
+            for i in range(extra_uneven_padding):
+                line[i] += " "
+            
+            # now we pad the evenly distributed ones
+            for i in range(words_need_padding):
+                line[i] += " " * padding_space
+            
+            # now we add the default one after each word by join with " "
+            return " ".join(line)
+        
+        ans = []
+        i = 0
+        while i < len(words):
+            newline = find_word(i)
+            i += len(newline)
+            ans.append(form_line(i, newline))
+
+        return ans
+
 
 class Solution:
     def fullJustify(self, words: List[str], maxWidth: int) -> List[str]:
