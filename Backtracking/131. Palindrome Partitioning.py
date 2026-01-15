a5c3c202dbc1e3c614e6f9293cbd9036bf3db24a
Shulin Ji <shulinji1024@gmail.com>
2026-01-14 19:21:31 -0500

    Update 131. Palindrome Partitioning.py

--- Backtracking/131. Palindrome Partitioning.py
+++ Backtracking/131. Palindrome Partitioning.py
@@ -17,6 +17,39 @@
 # 1 <= s.length <= 16
 # s contains only lowercase English letters.
 
+# SECOND ATTEMPT
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        def ispalindrome(curr_str):
+            i = 0
+            j = len(curr_str) - 1
+            while i < j:
+                if curr_str[i] != curr_str[j]:
+                    return False
+                i += 1
+                j -= 1
+            return True 
+
+        # we need a path because it is a partition, not to find all the palindromes!
+        # it is to find all possible ways of partitions
+        def dfs(string, path):
+            # if we exhausted the string, we successfully partitioned!
+            if not string:
+                ans.append(path[:])
+            
+            # we start with 1 because [:1] is the first single character to begin [:0] would be empty
+            for i in range(1, len(string) + 1):
+                # now if this part of the string is palindrome
+                if ispalindrome(string[:i]):
+                    # check the rest partition of the string by keep dfs
+                    path.append(string[:i])
+                    dfs(string[i:], path)
+                    path.pop()
+
+        ans = []
+        dfs(s, [])
+        return ans
+     
 
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
