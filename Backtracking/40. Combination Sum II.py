51a0f6806e3d1225af88cbe1ddd42cc422043e0d
Shulin Ji <shulinji1024@gmail.com>
2026-01-18 15:02:39 -0500

    Update 40. Combination Sum II.py

--- Backtracking/40. Combination Sum II.py
+++ Backtracking/40. Combination Sum II.py
@@ -32,6 +32,36 @@
 # 1 <= candidates[i] <= 50
 # 1 <= target <= 30
 
+# SECOND ATTEMPT
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        # O(2^n) and O(n)
+        def backtracking(curr_sum, path, start):
+            if curr_sum == target:
+                ans.add(tuple(path[:]))
+            # some early prune since all candidates are positive
+            elif curr_sum > target:
+                return
+
+            for i in range(start, len(candidates)):
+                # we prune the duplicate branches early, avoid cases like [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+                # and we use i > start so that we ensure that the start is actually being touched instead of prunning 
+                # every possible one, we still capture the case where all ones are used to achieve the atrget
+                if i > start and candidates[i] == candidates[i - 1]:
+                    continue
+                curr_sum += candidates[i]
+                path.append(candidates[i])
+                backtracking(curr_sum, path, i + 1)
+                curr_sum -= candidates[i]
+                path.pop()
+        
+        candidates.sort()
+        ans = set()
+        backtracking(0, [], 0)
+        return [list(x) for x in ans]
+
+
+
 # time limit exceeded on large test cases like 100 candidates with value 1 and target 30
 # O(2^n) time complexity in the worst case, where n is the number of candidates. This is because, in the worst case, we may explore all possible combinations of candidates.
 class Solution:
