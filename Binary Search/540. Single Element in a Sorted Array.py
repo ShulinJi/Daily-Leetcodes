bba28ac63c738b28919631f4f30f1452e62a5d1c
Shulin Ji <shulin.ji@mail.utoronto.ca>
2026-02-17 15:56:21 -0500

    added new problem

--- Binary Search/540. Single Element in a Sorted Array.py
+++ Binary Search/540. Single Element in a Sorted Array.py
@@ -21,6 +21,36 @@
 # 1 <= nums.length <= 105
 # 0 <= nums[i] <= 105
 
+# O(logn) and O(1)
+# SECOND ATTEMPT
+class Solution:
+    def singleNonDuplicate(self, nums: List[int]) -> int:
+        left = 0
+        right = len(nums) - 1
+        while left < right:
+            mid = left + (right - left) // 2
+            # if right side is even, 
+            right_side_even = (right - mid) % 2 == 0
+            # because we have only one single element and others are all pairs, then it means we always haveodd number of elements in the nums array, then [1,1,2,2,3], if mid == mid + 1, single element is at right side, [1,2,2,3,3], if mid == mid - 1, single element is at left side
+            if nums[mid] == nums[mid + 1]:
+                # if right side is even, then it means, after we take the mid + 1 away, the rest is odd, then the answer is on the right side
+                if right_side_even:
+                    left = mid + 2
+                else:
+                    right = mid - 1
+
+            # if right side is even and mid == mid - 1, then the answer is on left side because right side is even, there cannot be any single element, right = mid - 2 b/c we skipped the pair 2 element at a time
+            elif nums[mid] == nums[mid - 1]:
+                if right_side_even:
+                    right = mid - 2
+                else:
+                    left = mid + 1
+            else:
+                return nums[mid]
+        
+        return nums[left]
+
+
 class Solution:
     def singleNonDuplicate(self, nums: List[int]) -> int:
         # O(logn) and O(1)
