b86b6181d5b899c72dcbeb1814fb6672fe369996
Shulin Ji <shulinji1024@gmail.com>
2026-02-18 15:26:06 -0500

    Add binary search implementation in 704. Binary Search
    Implemented a binary search algorithm in the second attempt.

--- Binary Search/704. Binary Search.py
+++ Binary Search/704. Binary Search.py
@@ -12,6 +12,21 @@
 # Output: -1
 # Explanation: 2 does not exist in nums so return -1
 
+# SECOND ATTEMPT, template 1
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+        while left <= right:
+            mid = left + (right - left) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        
+        return -1
 
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
