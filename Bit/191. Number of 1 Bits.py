0882c8898e276f08b338389df3ff10118791f234
Shulin Ji <shulinji1024@gmail.com>
2026-02-20 16:58:24 -0500

    Implement optimized hammingWeight function
    Added a second attempt at the hammingWeight function using a clever bit manipulation technique, while retaining the original method for comparison.

--- Bit/191. Number of 1 Bits.py
+++ Bit/191. Number of 1 Bits.py
@@ -41,6 +41,40 @@
 
 # Follow up: If this function is called many times, how would you optimize it?
 
+# SECOND ATTEMPT
+
+# clever ones
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        count = 0
+        while n:
+            count += 1
+            # n−1 flips the least-significant 1-bit to 0.
+            # looping to remove the last bit that is 1
+            # n     = 1100
+            # n - 1 = 1011
+            # ---------------
+            # n & (n - 1) = 1000
+            n = n & (n - 1)
+        
+        return count 
+
+# Normal ones
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        # we use 1 to keep iterating the 32 bits integer to check the least significant bit
+        # and then we shift the mask to left one by one to check all 32 bits
+        # let's say we start at 00001, then shift left 00010 to check the second bit and so on
+        mask = 1
+        ans = 0
+        for i in range(32):
+            if n & mask != 0:
+                ans += 1
+            mask <<= 1
+        
+        return ans
+        
+
 
 class Solution:
     def hammingWeight(self, n: int) -> int:
