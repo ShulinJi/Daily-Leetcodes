e90fb25fa5ac6be3ebf20e12c8f2727195cb70cf
Shulin Ji <shulinji1024@gmail.com>
2026-03-12 17:39:31 -0400

    Add reverse function to Solution class
    Implement reverse function to reverse an integer while handling sign and overflow cases.

--- math/7. Reverse Integer.py
+++ math/7. Reverse Integer.py
@@ -21,3 +21,29 @@
 # Constraints:
 
 # -231 <= x <= 231 - 1
+
+
+class Solution:
+    def reverse(self, x: int) -> int:
+        # O(log(x)) because we are dividing by 0 each time
+        # leave the sign to avoid the exception of negative numbers
+        sign = 1 if x >= 0 else -1
+        reverse = 0
+        # we use abs value to use div mod
+        x = abs(x)
+
+        while x:
+            # 123, divmod(123, 10) is x = 12, mod = 3
+            x, mod = divmod(x, 10)
+            # then we keep doing the mod and it will form the reversed number
+            reverse = reverse * 10 + mod
+
+            # check for the negative bound
+            if reverse > 2**31 and not sign:
+                return 0
+            # check for the positive bound
+            if reverse > 2**31 - 1 and sign:
+                return 0
+            
+            
+        return sign * reverse
