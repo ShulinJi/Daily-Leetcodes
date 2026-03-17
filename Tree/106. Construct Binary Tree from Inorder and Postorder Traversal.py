3b7c69d06ab66e4e76d7657defe5485062e93737
Shulin Ji <shulin.ji@mail.utoronto.ca>
2026-03-17 16:15:16 -0400

    added new comments

--- Tree/106. Construct Binary Tree from Inorder and Postorder Traversal.py
+++ Tree/106. Construct Binary Tree from Inorder and Postorder Traversal.py
@@ -35,15 +35,19 @@ class Solution:
         # inorder left->root->right. postorder left->right->root preorder: root->left->right
         # postorder[-1] is the root
         def array_to_tree(left, right):
-            # use inorder as a bound so that we can regulate when to stop
             if left > right:
                 return None
             
-            # from back to front b/c the order that the root is at the end
+            # we contruct from the back since the root of postorder is at the end!
+            # similar with preorder and inorder, we both find the root and use the inorder to split left and right subtrees and then recursively build, except for postorder the root is at the back, we need to reverse the process!
             root_value = postorder.pop()
             root = TreeNode(root_value)
 
-            # construct right first b/c the order that we start from the back sot hat postorder: root->right-left
+            # then once we have the root, we need to know the left and right subtree!
+            # then we need the inorder! +1 is the right tree, -1 is the left subtree
+
+            # and we build the right side first because of the order of traversal, left->right->root, 
+            # reversed read from back become root, right, then left!
             root.right = array_to_tree(inorder_index_map[root_value] + 1, right) 
             root.left = array_to_tree(left, inorder_index_map[root_value] - 1)
 
