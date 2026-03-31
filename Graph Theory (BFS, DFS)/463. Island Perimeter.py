418fa6cae50f9e057068b6f97e97236732acaf15
Shulin Ji <shulin.ji@mail.utoronto.ca>
2026-03-31 12:41:47 -0400

    added new approach

--- Graph Theory (BFS, DFS)/463. Island Perimeter.py
+++ Graph Theory (BFS, DFS)/463. Island Perimeter.py
@@ -67,3 +67,39 @@ class Solution:
                     return ans
     
         return 0
+
+
+
+# better solution with O(m*n) and O(1), simple counting
+class Solution:
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        
+        rows = len(grid)
+        cols = len(grid[0])
+        
+        result = 0
+        
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 1:
+                    if r == 0:
+                        up = 0
+                    else:
+                        up = grid[r-1][c]
+                    if c == 0:
+                        left = 0
+                    else:
+                        left = grid[r][c-1]
+                    if r == rows-1:
+                        down = 0
+                    else:
+                        down = grid[r+1][c]
+                    if c == cols-1:
+                        right = 0
+                    else:
+                        right = grid[r][c+1]
+                        
+                    result += 4-(up+left+right+down)
+                
+        return result
+                
