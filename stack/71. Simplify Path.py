7d647bc8135ee61c668ad16eacde213d010ebef0
Shulin Ji <shulin.ji@mail.utoronto.ca>
2026-05-16 15:06:49 -0400

    added new problem

--- stack/71. Simplify Path.py
+++ stack/71. Simplify Path.py
@@ -1,3 +1,22 @@
+
+# SECOND ATTEMPT
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        stack = []
+        parts = path.split("/")
+
+        for p in parts:
+            if p == "" or p == ".":
+                continue
+            if p == "..":
+                if stack:
+                    stack.pop()
+            else:
+                stack.append(p)
+        
+        return "/" + "/".join(stack)
+
+
 class Solution:
     def simplifyPath(self, path: str) -> str:
         stack = []
